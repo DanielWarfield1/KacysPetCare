f72d9c414a993150d666493e4fc21b523b360d06
Daniel Warfield <51769447+DanielWarfield1@users.noreply.github.com>
2023-08-14 15:38:19 +0000

    Update 32 files

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,10 +1,16 @@
-import WindowSize from "../Components/WindowSize";
+import "../styles.css";
+import DogTitle from "../Components/DogTitle.js";
 import MadeByDaniel from "../Components/MadeByDaniel.js";
+import Prints from "../Components/Prints.js";
+import Sketch from "react-p5";
+import React from "react";
 
 export default function Services() {
   return (
     <div>
-      <WindowSize />
+      <DogTitle />
+      <Prints />
+      <div style={{ width: "1000px", height: "200px" }}></div>
       <MadeByDaniel />
     </div>
   );
